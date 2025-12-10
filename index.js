8d97f0ea3c83b10c54ea8ff959ba74b58420fa9b
raposinha <elena@raposinha.dev>
2025-12-10 16:43:08 +0100

    finishes until exercise 10

---
+++ index.js
@@ -0,0 +1,142 @@
+/* ESERCIZIO 1
+       Scrivi una funzione per cambiare il titolo della pagina in qualcos'altro
+    */
+
+const changeTitle = function () {
+  const h1 = document.querySelector("h1");
+  h1.innerText = "U1-W3-D32-DOM";
+};
+
+changeTitle();
+
+/* ESERCIZIO 2
+       Scrivi una funzione per aggiungere al titolo della pagina una classe "myHeading"
+    */
+
+const addClassToTitle = function () {
+  const h1 = document.querySelector("h1");
+  h1.classList.add("myHeading");
+};
+
+addClassToTitle();
+/* ESERCIZIO 3
+        Scrivi una funzione che cambi il testo dei p figli di un div
+      */
+
+const changePcontent = function () {
+  const paragraphs = document.querySelectorAll("div p");
+  paragraphs.forEach((p) => {
+    p.innerText = "Cambio di testo eseguito";
+  });
+};
+changePcontent();
+
+/* ESERCIZIO 4
+       Scrivi una funzione che cambi la proprietà href di ogni link (tranne quello nel footer) con il valore https://www.google.com
+      */
+
+const changeUrls = function () {
+  const links = document.querySelectorAll("a");
+  links.forEach((a) => {
+    a.href = "https://www.google.com";
+  });
+};
+
+changeUrls();
+
+/* ESERCIZIO 5
+       Scrivi una funzione che aggiunga un nuovo elemento lista alla seconda lista non ordinata
+    */
+
+const addToTheSecond = function () {
+  const secondUl = document.getElementById("secondList");
+  const li = document.createElement("li");
+  li.innerText = "4th";
+  secondUl.appendChild(li);
+};
+addToTheSecond();
+
+/* ESERCIZIO 6
+       Scrivi una funzione che aggiunga un paragrafo al primo div
+    */
+
+const addParagraph = function () {
+  const firstDiv = document.querySelector("div");
+  const newP = document.createElement("p");
+  newP.innerText = "sono un p dentro al div";
+  firstDiv.appendChild(newP);
+};
+addParagraph();
+
+/* ESERCIZIO 7
+       Scrivi una funzione che faccia scomparire la prima lista non ordinata
+    */
+
+const hideFirstUl = function () {
+  const firstUl = document.getElementById("firstList");
+  firstUl.style.display = "none";
+};
+hideFirstUl();
+
+/* ESERCIZIO 8 
+       Scrivi una funzione che renda verde il background di ogni lista non ordinata
+      */
+
+const paintItGreen = function () {
+  const allUl = document.querySelectorAll("ul");
+
+  allUl.forEach((ul) => (ul.style.background = "green"));
+};
+paintItGreen();
+
+/* ESERCIZIO 9
+       Scrivi una funzione che rimuova l'ultima lettera dall'h1 ogni volta che l'utente lo clicca
+      */
+
+const makeItClickable = function () {
+  const h1 = document.querySelector("h1");
+  h1.addEventListener("click", function () {
+    h1.innerText = h1.innerText.slice(0, -1);
+  });
+  h1.style.cursor = "pointer";
+};
+makeItClickable();
+
+/* ESERCIZIO 10
+       Crea una funzione che, al click sul footer, riveli l'URL del link interno come contenuto di un alert()
+      */
+
+const revealFooterLink = function () {
+  const footer = document.querySelector("footer");
+  const footerLink = document.querySelector("footer a");
+  footer.addEventListener("click", function () {
+    window.alert(footerLink.href);
+  });
+  footer.style.cursor = "pointer";
+};
+revealFooterLink();
+
+/* ESERCIZIO 11
+       Crea una funzione che crei una tabella nell'elemento con id "tableArea". 
+       La tabella avrà 5 elementi e questa struttura: immagine, nome prodotto, quantità, prezzo
+    */
+
+const generateTable = function () {};
+
+/* ESERCIZIO 12
+       Crea una funzione che aggiunga una riga alla tabella precedentemente creata e fornisca i dati necessari come parametri
+    */
+
+const addRow = function () {};
+
+/* ESERCIZIO 14
+      Crea una funzione che nasconda le immagini della tabella quando eseguita
+    */
+
+const hideAllImages = function () {};
+
+/* EXTRA ESERCIZIO 15
+      Crea una funzione che cambi il colore del h2 con id "changeMyColor" con un colore random ad ogni click ricevuto
+    */
+
+const changeColorWithRandom = function () {};
